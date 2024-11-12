179985392f3f2a08682b0385ab32b90f8609c680
melina-nikolaou <melina2000nikolaou@gmail.com>
2024-11-12 19:43:49 +0200

    Completed lab04

--- lab04.s
+++ lab04.s
@@ -30,6 +30,18 @@ str_ge:
 # Write the subroutine code here
 #  You may move jr ra   if you wish.
 #---------
+            lbu  t0, 0(a0)
+            lbu  t1, 0(a1)
+            sub  t2, t0,   t1  
+
+            addi a0, a0,   1
+            addi a1, a1,   1
+            add  t3, t1,   t0  
+            beq  t3, t0,   ret_strcmp  
+            beq  t2, zero, str_ge  #still equal, loop
+ret_strcmp:
+            srli a0, t2, 31  
+            xori a0, a0, 1
             jr   ra
  
 # ----------------------------------------------------------------------------
@@ -46,4 +58,26 @@ recCheck:
 # Write the subroutine code here
 #  You may move jr ra   if you wish.
 #---------
+            slti t0, a1,   2
+            beq  t0, zero, checkFirstTwo
+            addi a0, zero, 1  # return 1
+            jr   ra
+checkFirstTwo:
+            addi sp, sp,   -12
+            sw   ra, 8(sp)
+            sw   a0, 4(sp)
+            sw   a1, 0(sp)
+            lw   a1, 0(a0)  # 1st
+            lw   a0, 4(a0)  # 2nd 
+            jal  str_ge
+            beq  a0, zero, return  # return 0, a0 is already 0
+            # do recursion
+            lw   a0, 4(sp)    # get original a0, a1 from stack
+            lw   a1, 0(sp)
+            addi a0, a0,   4   # check the rest of the array, after 1st element.
+            addi a1, a1,   -1  # size-1
+            jal  recCheck
+return:
+            lw   ra, 8(sp)
+            addi sp, sp,   12
             jr   ra
